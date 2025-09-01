2208214e9846797da5fba6a5b36cb9a9df57e587
Joseph Ajireloja <40704218+josephajire@users.noreply.github.com>
2025-09-01 18:36:57 +0100

    Answer to question 2

---
+++ answer-to-question2.py
@@ -0,0 +1,38 @@
+# Base class representing a generic animal
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def move(self):
+        pass  # Subclasses should override this method
+
+
+# Dog subclass implementing move()
+class Dog(Animal):
+    def move(self):
+        print(f"{self.name} is Running")
+
+
+# Bird subclass implementing move()
+class Bird(Animal):
+    def move(self):
+        print(f"{self.name} is Flying")
+
+
+# Fish subclass implementing move()
+class Fish(Animal):
+    def move(self):
+        print(f"{self.name} is Swimming")
+
+
+# Function demonstrating polymorphism by calling move()
+def demonstrate_movement(animals):
+    for animal in animals:
+        animal.move()
+
+
+# List of animal instances with names
+animals = [Dog("Dog1"), Bird("Bird1"), Fish("Fish1")]
+
+print("Animals moving:")
+demonstrate_movement(animals)
